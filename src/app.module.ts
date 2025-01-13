870b395492d6e6c0d755c49c6e9c3093d63fda61
Aida <aidarile@gmail.com>
2025-01-13 13:55:50 +0100

    usamos el comando "nest generate resource Perros" para crear lo mismo que hicimos anteriormente, pero con perros

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GatosModule } from './gatos/gatos.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PerrosModule } from './perros/perros.module';
 
 @Module({
   imports: [
     GatosModule,
-    MongooseModule.forRoot("mongodb://localhost/gatos")
+    MongooseModule.forRoot("mongodb://localhost/gatos"),
+    PerrosModule
     ],
   controllers: [AppController],
   providers: [AppService],
